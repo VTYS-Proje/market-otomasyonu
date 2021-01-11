27664a448574f80b92762710a5d54ed1ff6c0320
mukaddes <mukad@DESKTOP-5TV477E>
2021-01-11 18:07:26 +0300

    borc sınıf

---
+++ Market_otomasyon/Moduls/Entity/Borc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_otomasyon.Moduls.Entity
+{
+    class Borc
+    {
+        public int IrsaliyeNo { get; set; }
+        public int TedarikciNo { get; set; }
+        public int Odeme { get; set; }
+        public int Tarih { get; set; }
+    }
+}
