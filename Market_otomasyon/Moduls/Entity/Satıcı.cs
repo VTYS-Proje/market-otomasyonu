112fdef7cb484974e6adc81662165ab0dbae5a78
Metleb99 <gemalmaz555@gmail.com>
2021-01-11 18:19:34 +0300

    Satıcı sınıfı eklendi

---
+++ Market_otomasyon/Moduls/Entity/Satıcı.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_otomasyon.Moduls.Entity
+{
+    public class Satıcı
+    {
+        public int SaticiID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+    }
+}
